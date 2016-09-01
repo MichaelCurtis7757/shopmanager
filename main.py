989b63aebc2ce090d39f9c7a5628da34a25aeb5e
Michael <aquausdev@gmail.com>
2016-09-02 00:07:50 +0100

    Added more story and added delays
    More story and realism added into the game. Fred now does more fish cooking stuff. Yeah.

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 #Shop Manager
+import time
 
 class GameInit():
     #handles inital startup
     def variables():
         #general
-        global day, cash, fish, potato
+        global day
+        global cash
+        
         day = 1
         cash = 0
         
         #stock
+        global fish
+        global potato
+        
         fish = 50
         cooked_fish = 0
         potato = 50
@@ -18,8 +24,8 @@ class GameInit():
     def manager_name():
         #handles the username
         global username
-        username = input("What is your name? ")
-        username_ch = input("Just to check, your name is '"+username+"', right? ")
+        username = input("Uncle Bob: What is your name, I always forget... ")
+        username_ch = input("Uncle Bob: Just to check, your name is '"+username+"', right? ")
         username_ch = username_ch.lower()
         if username_ch in ["yes", "y"]:
             return
@@ -27,13 +33,13 @@ class GameInit():
             GameInit.manager_name()
             return
         else:
-            print("Please enter a valid response!")
+            print("Game: Please enter a valid response!")
             GameInit.manager_name()
 
     @staticmethod  
     def startup():
         #runs all the startup stuff
-        pre_init = input("Would you like to start a new game? ")
+        pre_init = input("Game: Would you like to start a new game? ")
         pre_init = pre_init.lower()
         
         if pre_init in ["yes", "y"]:
@@ -45,7 +51,7 @@ class GameInit():
             SaveLoad.load()
             return
         else:
-            print("Please enter a valid response!")
+            print("Game: Please enter a valid response!")
             GameInit.startup()          
 
 class GameMain():
@@ -53,49 +59,62 @@ class GameMain():
     @staticmethod
     def main():
         while True:
-            print("Starting Game...")
+            print("Game: Starting Game...\n")
             GameMain.generic_day()
             break
 
-    def special_days():
-        #special day stuff
+    def generic_day():
+        #special days
         if day == 1:
             print("-= Day 1: The Beginning =-")
             print("Uncle Bob: Welcome to ShopManager! You must run the shop inherited from me, your Uncle Bob!")
-            return
+            time.sleep(1.5)
         
         if day == 7:
             print("-= Day 7: The First Week =-")
             print("Uncle Bob: Congratulations "+username+", you made it through the week. Here's an extra $250 to get you on your way!")
-            #day 7 cash bonus
             cash = cash + 250
-            return
-
-    def generic_day():
+            time.sleep(1.5)
+        
+        elif not (day == 1) or (day == 7):
+            print("-= Day "+str(day)+" =-")
+            time.sleep(1.5)
+        
         #start of day
         def fish_check():
+            global fish
             print("Fred: Mornin' Boss, how many fish do you want to cook this morning? ")
-            ask_fish = input("Fred: Oh, you currently have "+str(fish)+": ")
+            ask_fish = int(input("Fred: Oh, you currently have "+str(fish)+": "))
             if ask_fish > potato:
                 print("Fred: Boss, you can't cook more than you have!")
                 fish_check()
                 return
             else:
-                return
+                print("Fred: I'll cook the fish now Boss!")
+                time.sleep(1.5)
+                fish = fish - ask_fish
+                cooked_fish = ask_fish
+                print("Fred: Al'ight Boss, "+str(cooked_fish)+" were cooked!")
+            
+        fish_check()
         
         #middle section of day
 
         #end of day
-        print("Congratulations, you finished the day!")
+        print("Fred: Congratulations Boss, you finished the day!")
+        print("Fred: Sadly, all unsold food has to be thrown away.")
+        global day
+        day = day + 1
         def end():
-            ask_sav = input("Would you like to save your game? ")
+            ask_sav = input("Game: Would you like to save your game? ")
             ask_sav = ask_sav.lower()
             if ask_sav in ["yes", "y"]:
+                SaveLoad.save()
                 return
             elif ask_sav in ["no", "n"]:
                 return
             else:
-                print("Please enter a valid response!")
+                print("Game: Please enter a valid response!")
         end()
         
 class SaveLoad():
@@ -103,7 +122,7 @@ class SaveLoad():
     @staticmethod
     def save():
         #handles game saving
-        print("Saving Game...")
+        print("Game: Saving Game...")
         file = open("data.txt", "w")
         file.write(username)
         file.write("\n")
@@ -115,12 +134,12 @@ class SaveLoad():
         file.write("\n")
         file.write(str(potato))
         file.write("\n")
-        print("Game Saved!")
+        print("Game: Game Saved!")
 
     @staticmethod
     def load():
         #handles game loading
-        print("Loading Game...")
+        print("Game: Loading Game...")
 
         file = open("data.txt", "r")
         #username
@@ -143,7 +162,6 @@ class SaveLoad():
         load_potato = file.readline().replace("\n", "")
         potato = load_potato
 
-        #temp code below
-        print(load_data)
+        print("Game: Game Loaded!")
         
 GameInit.startup()
